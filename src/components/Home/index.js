3c48bd48f8890a746503e420e253957c846867e7
wanyingtian <wanying.tian@outlook.com>
2022-10-23 23:05:59 -0700

    completed home page with animation!

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,17 +1,67 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+import AnimatedLetters from '../AnimatedLetters'
 import LogoS from '../../assets/images/W.png'
-import { Link} from 'react-router-dom'
-import './index.scss';
+
+import './index.scss'
+
+
+
 const Home = () => {
 
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['a','n','y','i','n','g']
+    const jobArray = [
+        'b',
+        'i',
+        'o',
+        'm',
+        'e',
+        'd',
+        'i',
+        'c',
+        'a',
+        'l',
+        ' ',
+
+        'e',
+        'n',
+        'g',
+        'i',
+        'n',
+        'e',
+        'e',
+        'r',
+        '.',
+    ]
+
+
+    useEffect(() => {
+        return () =>setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000);
+    },[]); 
 
     return (
+        
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br /> I'm 
+                <h1>
+                <span className={letterClass}>H</span>
+                <span className={`${letterClass} _12`}>i,</span>   
+                <br /> 
+                <span className={`${letterClass} _13`}>I</span>  
+                <span className={`${letterClass} _14`}> 'm
+                </span>  
                 <img src={LogoS} alt ="developer" />
-                anying
+                <AnimatedLetters letterClass= {letterClass}
+                strArray = {nameArray}
+                idx = {15}/>
                 <br />
-                biomedical engineer
+                <AnimatedLetters letterClass={letterClass}
+                strArray = {jobArray}
+                idx = {16}/>
                 </h1>
                 <h2>Data Science & Machine Learning Enthusiast</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
@@ -19,6 +69,10 @@ const Home = () => {
 
         </div>
     );
+
+
+
+
 }
 
 export default Home
